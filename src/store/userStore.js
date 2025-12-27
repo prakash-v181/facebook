b81142087bfcd8f86cff85a0b68c422213f403a9
prakash-v181 <prakashtare110@outlook.com>
2025-12-27 15:22:50 +0530

    fix

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,18 +1,41 @@
-import { create } from 'zustand'
-import {persist} from 'zustand/middleware'
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const userStore = create(
-    persist(
-        (set) =>({
-            user:null,
-            setUser:( userData) => set({user:userData}),
-            clearUser : () => set({user:null})
-        }),
-        {
-            name: "user-storage",
-            getStorage: () => localStorage,
-        }
-    )
-)
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (userData) => set({ user: userData }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: "user-storage", // Storage key
+      storage: createJSONStorage(() => localStorage), // Updated API
+    }
+  )
+);
 
-export default userStore;
+export default useUserStore;
+
+
+
+
+
+// import { create } from 'zustand'
+// import {persist} from 'zustand/middleware'
+
+// const userStore = create(
+//     persist(
+//         (set) =>({
+//             user:null,
+//             setUser:( userData) => set({user:userData}),
+//             clearUser : () => set({user:null})
+//         }),
+//         {
+//             name: "user-storage",
+//             getStorage: () => localStorage,
+//         }
+//     )
+// )
+
+// export default userStore;
